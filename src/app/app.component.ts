a024b91ee6757bfb2cf8ac13331f1f9d4da1610e
Adán González Rodríguez <adan.gonzalez@clickdimensions.com>
2023-08-18 10:03:16 +0100

    Reactive: Creating Custom Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +9,15 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female', 'soy un perro'];
   signupForm: FormGroup;
+  forbiddenUserNames = ['caca', 'pedo'];
 
+  constructor(private formBuilder: FormBuilder) { }
   ngOnInit(): void {
-
     //Called after the constructor, initializing input pr operties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('soy un perro'),
@@ -34,5 +35,12 @@ export class AppComponent implements OnInit {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
     console.log(this.signupForm.get('hobbies').value);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) != -1) {
+      return { 'nameIsForbidden': true }
+    }
+    return null;
   }
 }
