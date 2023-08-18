50bbca0acc0e89368ae3f9e8f7cedbc6e70cdfc1
Adán González Rodríguez <adan.gonzalez@clickdimensions.com>
2023-08-18 08:10:30 +0100

    Fixing a Bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,10 @@ export class AppComponent implements OnInit {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
       'gender': new FormControl('soy un perro')
     });
   }
